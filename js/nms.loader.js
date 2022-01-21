e099928113286c56d249768d6fedbe6e839b706f
Alex Ashmore <Kezyma@yahoo.co.uk>
2022-01-21 16:08:27 +0000

    Update nms.loader.js

--- js/nms.loader.js
+++ js/nms.loader.js
@@ -2,10 +2,10 @@ var data = {
     Ships: {
         Starter: [
             {
-                File: "data/nms/Ships/Radiant Pillar BC1.shp",
+                File: "/data/nms/Ships/Radiant Pillar BC1.shp",
                 Name: "Radiant Pillar BC1",
                 AltName: "",
-                StatImage: "img/nms/Radiant Pillar BC1.jpg",
+                StatImage: "/img/nms/Radiant Pillar BC1.jpg",
                 Screenshot: "",
                 Description: ""
             }
@@ -38,32 +38,34 @@ function bindShips(category, data) {
     var container = $("div[data-type='Ships'][data-type='" + category + "']");
     for (var ix in data) {
         var ship = data[ix];
-        var shipJson = $.getJSON(ship.File);
-        var shipClass = shipJson.responseJSON.Ship["@Cs"][";l5"]["B@N"]["1o6"];
-        var damage = 0, shield = 0, hyperdrive = 0;
-        var shipStats = data.responseJSON.Ship["@Cs"][";l5"]["@bB"];
-        for (var stat in shipStats) {
-            var statVal = stat[">MX"]
-            switch (stat["QL1"]) {
-                case "^SHIP_DAMAGE":
-                    damage = statVal;
-                    break;
-                case "^SHIP_SHIELD":
-                    shield = statVal;
-                    break;
-                case "^SHIP_HYPERDRIVE":
-                    hyperdrive = statVal;
-                    break;
+        $.getJSON(ship.File, function (shipJson) {
+            console.log(shipJson);
+            var shipClass = shipJson.responseJSON.Ship["@Cs"][";l5"]["B@N"]["1o6"];
+            var damage = 0, shield = 0, hyperdrive = 0;
+            var shipStats = data.responseJSON.Ship["@Cs"][";l5"]["@bB"];
+            for (var stat in shipStats) {
+                var statVal = stat[">MX"]
+                switch (stat["QL1"]) {
+                    case "^SHIP_DAMAGE":
+                        damage = statVal;
+                        break;
+                    case "^SHIP_SHIELD":
+                        shield = statVal;
+                        break;
+                    case "^SHIP_HYPERDRIVE":
+                        hyperdrive = statVal;
+                        break;
+                }
             }
-        }
-        var template = "<div class='col'>";
-        template += "<div class='card'>";
-        template += "<div class='card-header'>";
-        template += "<img src='/img/nms/" + shipClass + ".png' />";
-        template += "<h5>" + ship.Name + "</h5>";
-        template += "</div>";
-        template += "</div>";
-        template += "</div>";
-        container.append($(template));
+            var template = "<div class='col'>";
+            template += "<div class='card'>";
+            template += "<div class='card-header'>";
+            template += "<img src='/img/nms/" + shipClass + ".png' />";
+            template += "<h5>" + ship.Name + "</h5>";
+            template += "</div>";
+            template += "</div>";
+            template += "</div>";
+            container.append($(template));
+        });
     }
 }
