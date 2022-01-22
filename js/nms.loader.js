09408f55182ff73cb62192a77858cdb728da7205
Alex Ashmore <Kezyma@yahoo.co.uk>
2022-01-22 16:52:00 +0000

    nms wip

--- js/nms.loader.js
+++ js/nms.loader.js
@@ -50,7 +50,7 @@ var data = {
                 AltNames: "",
                 StatImage: "/img/nms/Hadach's Discovery KH3 (Expedition).jpg",
                 Screenshot: "",
-                Description: "Reward from Expedition 2: Beachhead Phase 2 Completion.",
+                Description: "Phase 2 reward from Expedition 2: Beachhead.",
                 Missing: false
             },
             {
@@ -162,6 +162,20 @@ var data = {
                 Description: "Starter multitool."
             }
         ],
+        PreOrder: [
+            {
+                Name: "Rezosu Z65",
+                Missing: true
+            }, 
+            {
+                Name: "Artios-VI",
+                Missing: true
+            },
+            {
+                Name: "Vertic-LX",
+                Missing: true
+            }
+        ],
         Expeditions: [
             {
                 File: "/data/nms/Multitools/Geometric Dream Inverter.mlt",
@@ -169,7 +183,7 @@ var data = {
                 AltNames: "",
                 StatImage: "/img/nms/Geometric Dream Inverter.jpg",
                 Screenshot: "",
-                Description: ""
+                Description: "Phase 5 reward from Expedition 1: The Pioneers."
             },
             {
                 File: "/data/nms/Multitools/Surge of Storms B56-K1-6.mlt",
@@ -177,7 +191,7 @@ var data = {
                 AltNames: "",
                 StatImage: "/img/nms/Surge of Storms B56-K1-6.jpg",
                 Screenshot: "",
-                Description: ""
+                Description: "Phase 2 reward from Expedition 1: The Pioneers."
             }
         ],
         Twitch: [
@@ -187,7 +201,7 @@ var data = {
                 AltNames: "",
                 StatImage: "/img/nms/Arc Capacitor W_17I-15P.jpg",
                 Screenshot: "",
-                Description: ""
+                Description: "Twitch Season 1, Day 3 drop."
             },
             {
                 File: "/data/nms/Multitools/Hunevar's Dream Charger.mlt",
@@ -195,7 +209,7 @@ var data = {
                 AltNames: "",
                 StatImage: "/img/nms/Hunevar's Dream Charger.jpg",
                 Screenshot: "",
-                Description: ""
+                Description: "Twitch Season 1, Day 4 drop."
             },
             {
                 File: "/data/nms/Multitools/Imperfect Loop Reflector.mlt",
@@ -203,7 +217,7 @@ var data = {
                 AltNames: "",
                 StatImage: "/img/nms/Imperfect Loop Reflector.jpg",
                 Screenshot: "",
-                Description: ""
+                Description: "Twitch Season 1, Day 5 drop."
             },
             {
                 File: "/data/nms/Multitools/Loop Scoop Mark IV.mlt",
@@ -211,7 +225,7 @@ var data = {
                 AltNames: "",
                 StatImage: "/img/nms/Loop Scoop Mark IV.jpg",
                 Screenshot: "",
-                Description: ""
+                Description: "Twitch Season 1, Day 2 drop."
             },
             {
                 File: "/data/nms/Multitools/Quantum Harmoniser Mark IV.mlt",
@@ -219,13 +233,79 @@ var data = {
                 AltNames: "",
                 StatImage: "/img/nms/Quantum Harmoniser Mark IV.jpg",
                 Screenshot: "",
-                Description: ""
+                Description: "Twitch Season 1, Day 1 drop."
             }
         ]
     },
     Companions: {
-        Expeditions: [],
-        Twitch: []
+        Expeditions: [
+            {
+                File: "/data/nms/Companions/Burrowing Monstrosity.cmp",
+                Name: "Burrowing Monstrosity",
+                AltNames: "Biological Horror",
+                StatImage: "/img/nms/Burrowing Monstrosity.jpg",
+                Screenshot: "",
+                Description: ""
+            },
+            {
+                File: "/data/nms/Companions/D. Tempookorosa.cmp",
+                Name: "D. Tempookorosa",
+                AltNames: "",
+                StatImage: "/img/nms/D. Tempookorosa.jpg",
+                Screenshot: "",
+                Description: ""
+            },
+            {
+                File: "/data/nms/Companions/Maggotling.cmp",
+                Name: "Maggotling",
+                AltNames: "Hungering Worm",
+                StatImage: "/img/nms/Maggotling.jpg",
+                Screenshot: "",
+                Description: ""
+            }
+        ],
+        Twitch: [
+            {
+                File: "/data/nms/Companions/G. Oestridinzeus.cmp",
+                Name: "G. Oestridinzeus",
+                AltNames: "",
+                StatImage: "/img/nms/G. Oestridinzeus.jpg",
+                Screenshot: "",
+                Description: ""
+            },
+            {
+                File: "/data/nms/Companions/J. Croconakeum.cmp",
+                Name: "J. Croconakeum",
+                AltNames: "",
+                StatImage: "/img/nms/J. Croconakeum.jpg",
+                Screenshot: "",
+                Description: ""
+            },
+            {
+                File: "/data/nms/Companions/P. Sheptefalae.cmp",
+                Name: "P. Sheptefalae",
+                AltNames: "",
+                StatImage: "/img/nms/P. Sheptefalae.jpg",
+                Screenshot: "",
+                Description: ""
+            },
+            {
+                File: "/data/nms/Companions/V. Pentaamqum.cmp",
+                Name: "V. Pentaamqum",
+                AltNames: "",
+                StatImage: "/img/nms/V. Pentaamqum.jpg",
+                Screenshot: "",
+                Description: ""
+            },
+            {
+                File: "/data/nms/Companions/Z. Falynascria.cmp",
+                Name: "Z. Falynascria",
+                AltNames: "",
+                StatImage: "/img/nms/Z. Falynascria.jpg",
+                Screenshot: "",
+                Description: ""
+            }
+        ]
     }
 };
 
@@ -239,8 +319,11 @@ function bindData() {
     bindShips("Expeditions", data.Ships.Expeditions);
     bindShips("Twitch", data.Ships.Twitch);
     bindMultitools("Starter", data.Multitools.Starter);
+    bindMultitools("PreOrder", data.Multitools.PreOrder);
     bindMultitools("Expeditions", data.Multitools.Expeditions);
     bindMultitools("Twitch", data.Multitools.Twitch);
+    bindCompanions("Expeditions", data.Companions.Expeditions);
+    bindCompanions("Twitch", data.Companions.Twitch);
 }
 
 function bindShip(ship, container) {
@@ -312,45 +395,92 @@ function bindShip(ship, container) {
 }
 
 function bindMultitool(multitool, container) {
-    $.getJSON(multitool.File, function (toolJson) {
-        var toolName = multitool.Name;
-        var toolAltName = multitool.AltNames;
-        var toolStatImg = multitool.StatImage;
-        var toolClass = toolJson.MultiTool["OsQ"]["B@N"]["1o6"];
-        var toolSeed = toolJson.MultiTool["CA4"]["@EL"][1];
-        var toolSlots = toolJson.MultiTool["OsQ"]["hl?"].length;
-        var toolStats = toolJson.MultiTool["OsQ"]["@bB"];
-        var damage = 0.0, mining = 0.0, scan = 0.0;
-        for (var stat in toolStats) {
-            var statVal = toolStats[stat][">MX"]
-            switch (toolStats[stat]["QL1"]) {
-                case "^WEAPON_DAMAGE":
-                    damage = statVal;
-                    break;
-                case "^WEAPON_MINING":
-                    mining = statVal;
-                    break;
-                case "^WEAPON_SCAN":
-                    scan = statVal;
-                    break;
-            }
-        }
-        var table = "<table class='table table-sm m-0'>"
-        table += "<tbody>";
-        if (toolAltName != null && toolAltName != "") {
-            table += "<tr><td>Alt Name</td><td>" + toolAltName + "</td></tr>";
-        }
-        //table += "<tr><td>Type</td><td>" + shipType + "</td></tr>";
-        table += "<tr><td>Seed</td><td>" + toolSeed + "</td></tr>";
-        table += "<tr><td>Slots</td><td>" + toolSlots + "</td></tr>";
-        table += "<tr><td>Damage Bonus</td><td>" + damage + "</td></tr>";
-        table += "<tr><td>Mining Bonus</td><td>" + mining + "</td></tr>";
-        table += "<tr><td>Scanning Bonus</td><td>" + scan + "</td></tr>";
-        table += "</tbody>";
-        table += "</table>";
-        var template = getCard(toolName, toolClass, multitool.Description, table, toolStatImg, multitool.File);
+    var toolName = multitool.Name;
+    if (multitool.Missing) {
+        var template = getMissing(toolName);
         container.append($(template));
-    });
+    }
+    else {
+        $.getJSON(multitool.File, function (toolJson) {
+            var toolAltName = multitool.AltNames;
+            var toolStatImg = multitool.StatImage;
+            var toolClass = toolJson.MultiTool["OsQ"]["B@N"]["1o6"];
+            var toolSeed = toolJson.MultiTool["CA4"]["@EL"][1];
+            var toolSlots = toolJson.MultiTool["OsQ"]["hl?"].length;
+            var toolStats = toolJson.MultiTool["OsQ"]["@bB"];
+            var damage = 0.0, mining = 0.0, scan = 0.0;
+            for (var stat in toolStats) {
+                var statVal = toolStats[stat][">MX"]
+                switch (toolStats[stat]["QL1"]) {
+                    case "^WEAPON_DAMAGE":
+                        damage = statVal;
+                        break;
+                    case "^WEAPON_MINING":
+                        mining = statVal;
+                        break;
+                    case "^WEAPON_SCAN":
+                        scan = statVal;
+                        break;
+                }
+            }
+            var table = "<table class='table table-sm m-0'>"
+            table += "<tbody>";
+            if (toolAltName != null && toolAltName != "") {
+                table += "<tr><td>Alt Name</td><td>" + toolAltName + "</td></tr>";
+            }
+            table += "<tr><td>Seed</td><td>" + toolSeed + "</td></tr>";
+            table += "<tr><td>Slots</td><td>" + toolSlots + "</td></tr>";
+            table += "<tr><td>Damage Bonus</td><td>" + damage + "</td></tr>";
+            table += "<tr><td>Mining Bonus</td><td>" + mining + "</td></tr>";
+            table += "<tr><td>Scanning Bonus</td><td>" + scan + "</td></tr>";
+            table += "</tbody>";
+            table += "</table>";
+            var template = getCard(toolName, toolClass, multitool.Description, table, toolStatImg, multitool.File);
+            container.append($(template));
+        });
+    }
+}
+
+function bindCompanion(companion, container) {
+    var compName = companion.Name;
+    if (companion.Missing) {
+        var template = getMissing(compName);
+        container.append($(template));
+    }
+    else {
+        $.getJSON(companion.File, function (compJson) {
+            var compAltName = companion.AltNames;
+            var compStatImg = companion.StatImage;
+            var compClass = "N";
+            var compType = compJson.Companion["HbY"]["HbY"];
+            var compBiome = compJson.Companion["8jm"]["8jm"];
+            var compCreatureSeed = compJson.Companion["WTp"][1];
+            var compCreature2ndSeed = compJson.Companion["1p="][1];
+            var compSpeciesSeed = compJson.Companion["m9o"];
+            var compGenusSeed = compJson.Companion["JrL"];
+            var compHelpfulness = compJson.Companion["JAy"][0];
+            var compGentleness = compJson.Companion["JAy"][1];
+            var compIndependence = compJson.Companion["JAy"][2];
+
+            var table = "<table class='table table-sm m-0'>"
+            table += "<tbody>";
+            if (compAltName != null && compAltName != "") {
+                table += "<tr><td>Alt Name</td><td>" + compAltName + "</td></tr>";
+            }
+            table += "<tr><td>Biome-Type</td><td>" + compBiome + " - " + compType + "</td></tr>";
+            table += "<tr><td>Creature Seed</td><td>" + compCreatureSeed + "</td></tr>";
+            table += "<tr><td>Creature 2nd Seed</td><td>" + compCreature2ndSeed + "</td></tr>";
+            table += "<tr><td>Genus Seed</td><td>" + compGenusSeed + "</td></tr>";
+            table += "<tr><td>Species Seed</td><td>" + compSpeciesSeed + "</td></tr>";
+            table += "<tr><td>Helpfulness</td><td>" + compHelpfulness + "</td></tr>";
+            table += "<tr><td>Gentleness</td><td>" + compGentleness + "</td></tr>";
+            table += "<tr><td>Independence</td><td>" + compIndependence + "</td></tr>";
+            table += "</tbody>";
+            table += "</table>";
+            var template = getCard(compName, compClass, companion.Description, table, compStatImg, companion.File);
+            container.append($(template));
+        });
+    }
 }
 
 function getCard(name, cl, desc, table, img, file) {
@@ -390,5 +520,13 @@ function bindMultitools(category, data) {
     for (var ix in data) {
         var multitool = data[ix];
         bindMultitool(multitool, container);
+    }
+}
+
+function bindCompanions(category, data) {
+    var container = $("div[data-type='Companions'][data-category='" + category + "']");
+    for (var ix in data) {
+        var companion = data[ix];
+        bindCompanion(companion, container);
     }
 }
