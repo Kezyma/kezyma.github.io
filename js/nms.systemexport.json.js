0e042e9daf00d0cd7c51877fba64acdd7ed36f39
Alex Ashmore <Kezyma@yahoo.co.uk>
2022-01-28 13:13:45 +0000

    Update nms.systemexport.json.js

--- js/nms.systemexport.json.js
+++ js/nms.systemexport.json.js
@@ -25,17 +25,27 @@ function ExportData() {
             console.log("Checking " + system.Seed + " Planet " + j);
             var grass = PlanetTable.rows[14].cells[j].getElementsByTagName("table")[0].rows[0].cells;
             var grassNums = []
-            for (var g = 0; g < grass.length; g++){ grassNums.push(grass[g].innerText);}
+            for (var g = 0; g < grass.length; g++){ grassNums.push(parseInt(grass[g].innerText));}
             var plant = PlanetTable.rows[15].cells[j].getElementsByTagName("table")[0].rows[0].cells;
             var plantNums = []
-            for (var g = 0; g < plant.length; g++){ plantNums.push(plant[g].innerText);}
+            for (var g = 0; g < plant.length; g++){ plantNums.push(parseInt(plant[g].innerText));}
             var leaf = PlanetTable.rows[16].cells[j].getElementsByTagName("table")[0].rows[0].cells;
             var leafNums = []
-            for (var g = 0; g < leaf.length; g++){ leafNums.push(leaf[g].innerText);}
+            for (var g = 0; g < leaf.length; g++){ leafNums.push(parseInt(leaf[g].innerText));}
             var tile = PlanetTable.rows[18].cells[j].getElementsByTagName("table")[0].rows[0].cells;
             var tileNums = []
-            for (var g = 0; g < tile.length; g++){ tileNums.push(tile[g].innerText);}
+            for (var g = 0; g < tile.length; g++){ tileNums.push(parseInt(tile[g].innerText));}
 
+            var water = false;
+            if (parseInt(PlanetTable.rows[20].cells[j].innerText) == 1) { water = true; }
+            var prime = false;
+            if (parseInt(PlanetTable.rows[27].cells[j].innerText) == 1) { prime = true; }
+            var continents = false;
+            if (parseInt(PlanetTable.rows[28].cells[j].innerText) == 1) { continents = true; }
+            var rings = false;
+            if (parseInt(PlanetTable.rows[29].cells[j].innerText) == 1) { rings = true; }
+            var sandworms = false;
+            if (parseInt(PlanetTable.rows[30].cells[j].innerText) == 1) { sandworms = true; }
             var planet = {
                 Biome: PlanetTable.rows[1].cells[j].innerText,
                 SubBiome: PlanetTable.rows[2].cells[j].innerText,
@@ -51,7 +61,7 @@ function ExportData() {
                 StormScreenFilter: PlanetTable.rows[11].cells[j].innerText.split("/")[1],
                 Atmosphere: PlanetTable.rows[12].cells[j].innerText,
                 Rainbow: PlanetTable.rows[13].cells[j].innerText,
-                Water: PlanetTable.rows[20].cells[j].innerText,
+                Water: water,
                 WaterColour: PlanetTable.rows[19].cells[j].innerText,
                 Resources: PlanetTable.rows[21].cells[j].innerText,
                 CommonSubstance: PlanetTable.rows[22].cells[j].innerText,
@@ -59,12 +69,12 @@ function ExportData() {
                 RareSubstance: PlanetTable.rows[24].cells[j].innerText,
                 BuildingLevel: PlanetTable.rows[25].cells[j].innerText,
                 Type: PlanetTable.rows[26].cells[j].innerText,
-                Prime: PlanetTable.rows[27].cells[j].innerText,
-                ForceContinents: PlanetTable.rows[28].cells[j].innerText,
-                Rings: PlanetTable.rows[29].cells[j].innerText,
-                Sandworms: PlanetTable.rows[30].cells[j].innerText,
-                CreatureNumber: PlanetTable.rows[31].cells[j].innerText,
-                SkyColour: PlanetTable.rows[17].cells[j].innerText,
+                Prime: prime,
+                ForceContinents: continents,
+                Rings: rings,
+                Sandworms: sandworms,
+                CreatureNumber: parseInt(PlanetTable.rows[31].cells[j].innerText),
+                SkyColour: parseInt(PlanetTable.rows[17].cells[j].innerText),
                 GrassColours: grassNums,
                 PlantColours: plantNums,
                 LeafColours: leafNums,
@@ -82,13 +92,14 @@ function ExportData() {
                         Value: PlanetCreatureSubTable.rows[1].cells[l].innerText,
                         Type:PlanetCreatureSubTable.rows[2].cells[l].innerText,
                         Size:PlanetCreatureSubTable.rows[3].cells[l].innerText.split("/")[0],
-                        Weight:PlanetCreatureSubTable.rows[3].cells[l].innerText.split("/")[1],
-                        Height:PlanetCreatureSubTable.rows[3].cells[l].innerText.split("/")[2],
+                        Weight: parseFloat(PlanetCreatureSubTable.rows[3].cells[l].innerText.split("/")[1].replace('kg','')),
+                        Height: parseFloat(PlanetCreatureSubTable.rows[3].cells[l].innerText.split("/")[2].replace('m','')),
                         Density: PlanetCreatureSubTable.rows[4].cells[l].innerText.split("/")[0],
-                        GroupSize: PlanetCreatureSubTable.rows[4].cells[l].innerText.split("/")[1],
-                        GroupsKm2: PlanetCreatureSubTable.rows[4].cells[l].innerText.split("/")[2],
+                        MinGroup: parseInt(PlanetCreatureSubTable.rows[4].cells[l].innerText.split("/")[1].split("-")[0]),
+                        MaxGroup: parseInt(PlanetCreatureSubTable.rows[4].cells[l].innerText.split("/")[1].split("-")[1]),
+                        GroupsKm2: parseInt(PlanetCreatureSubTable.rows[4].cells[l].innerText.split("/")[2]),
                         Rarity: PlanetCreatureSubTable.rows[6].cells[l].innerText.split("/")[0],
-                        Probability: PlanetCreatureSubTable.rows[6].cells[l].innerText.split("/")[1],
+                        Probability: parseFloat(PlanetCreatureSubTable.rows[6].cells[l].innerText.split("/")[1].replace('%',''))/100,
                         Active: PlanetCreatureSubTable.rows[5].cells[l].innerText.split("/")[0],
                         Terrain: PlanetCreatureSubTable.rows[5].cells[l].innerText.split("/")[1],
                         Hemisphere: PlanetCreatureSubTable.rows[5].cells[l].innerText.split("/")[2],
